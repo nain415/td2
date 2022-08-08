4a835c7c313caf37296ec639e3f4ff5ab26e6b97
Nain <justin@hnain.dev>
2022-08-07 19:03:27 -0700

    populated db of players

--- td2.py
+++ td2.py
@@ -1,13 +1,14 @@
 import config
-import requests
-import pandas, sqlite3
+import requests, pandas, time
+import db_fns
 from bs4 import BeautifulSoup as bs
 
 headers = {'x-api-key':config.API_KEY, 'accept': 'application/json'}
 API_URL = config.API_URL
+LIMIT = 1000
 
 
-#expects lim > 0 in integers
+#expects 1000 >= lim > 0 in integers
 #offset > 0 in integers, optional
 def player_stats(lim, offset=""):
     if offset:
@@ -37,12 +38,43 @@ def game_stats(start_date, end_date, lim, offset=""):
     re = requests.get(URL, headers=headers)
     return re.json()
 
+
+#takes player statisics and converts to table-insertable form
+#also some defensive if cases
 def shape(stats):
     for num in range(len(stats)):
-        stats[num]['playerName'] = stats[num]['profile'][0]['playerName']
+        if len(stats[num]['profile']) == 0:
+            stats[num]['playerName'] = ""
+
+        else:
+            stats[num]['playerName'] = stats[num]['profile'][0]['playerName']
+        
+        if 'gamesPlayed' not in stats[num]:
+            stats[num]['gamesPlayed'] = 0
+        
+        if 'secondsPlayed' not in stats[num]:
+            stats[num]['secondsPlayed'] = 0
 
 
+#query limit of 1000
+#start and end are indices.  will fetch in multiples of 1000
+#in range [start, end] inclusive.
+#example populate_players(1,2)
+def populate_players(start, end):
+    try:
+        for i in range(start,end+1):
+            t = time.time()
 
+            players = player_stats(LIMIT, i*LIMIT)
+            shape(players)
+            db_fns.ins('stats', players)
+
+            print(f"inserted {i*LIMIT} to {(i+1)*LIMIT}.")
+            print(f"It took {time.time() - t} seconds taken to do this.")
+
+    except Exception as e:
+        print(e)
+        print('Done populating.  Program crashed.')
 
 
 
