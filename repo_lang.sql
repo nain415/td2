1d9c27fdf88799336640e57e1b73830ea8f10a24
Nain415 <78244259+Nain415@users.noreply.github.com>
2022-08-13 19:27:42 -0700

    Update repo_lang.sql

--- repo_lang.sql
+++ repo_lang.sql
@@ -1,5 +1,1 @@
 --corrects github repo language
-CREATE TABLE IF NOT EXISTS playermatch (player_id TEXT REFERENCES player(id), match_id TEXT REFERENCES match(id), playerSlot INTEGER, legion TEXT, workers INTEGER, value INTEGER,
-    gameResult TEXT, classicElo INTEGER, chosenSpell TEXT, firstWaveFighters TEXT,
-    PRIMARY KEY(player_id, match_id)
-    );
